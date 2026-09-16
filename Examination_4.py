2f84300cc93d6fb089b0d3877a76d275b7fa725a
0838759904 <28113@ud.ac.th>
2026-09-16 11:10:13 +0700

    Update Examination_4.py

--- Examination_4.py
+++ Examination_4.py
@@ -7,4 +7,19 @@
 
 
 # นักเรียนเขียนโค้ดต่อจากบรรทัดนี้
-score = int(input())
+score = int(input(กรอกคะแนน: ))
+
+if score >=80:
+    grade = "A"
+elif score >=70:
+    grade = "B"
+elif score >=60:
+    grade = "C"
+elif score >=50:
+    grade = "D"
+
+else:
+    grade = "F"
+
+score = int(input("กรอกคะแนน: , score"))
+
